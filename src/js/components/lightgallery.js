3a94a20289209346ac4c67d37f985b0cbb4dc104
NaLi831 <lkqw0669@gmail.com>
2025-12-16 14:32:06 +0300

    feat: создание lighgallery

--- src/js/components/lightgallery.js
+++ src/js/components/lightgallery.js
@@ -5,7 +5,7 @@ import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.umd.js'
 import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.umd.js'
 
  lightGallery(document.getElementById('lightgallery'), {
-        plugins: [lgZoom, lgThumbnail],
-        speed: 500,
+  plugins: [lgZoom, lgThumbnail],
+  speed: 500,
 });
 
